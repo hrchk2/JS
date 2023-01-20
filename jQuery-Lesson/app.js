8919e69ed01ac53399f6ee2ea62ee54f2b6f23a7
EC2 Default User <amrumineq@gmail.com>
2023-01-20 05:08:06 +0000

    practice

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -28,3 +28,40 @@ $(function(){
     });
   });
 });
+
+
+$(function() {
+  $('.box4').mouseover(function(){
+    $('.box4').addClass('box4-ext');
+  });
+  $('.box4').mouseout(function(){
+    $('.box4').removeClass('box4-ext');
+  });
+});
+
+
+$(function() {
+  $('.box5').on('click',function(){
+    $('.box5').addClass('box5-ext').on('click',function(){
+        $('.box5').removeClass('box5-ext');
+    });
+  });
+});
+
+// 連続クリック防止フラグ
+let clickFlg = true;
+$(function() {
+  $(".box6").on("click", function() {
+    if(clickFlg) {
+    // イベント処理中はフラグをoffにします。
+      clickFlg = false;
+      $('.box6').addClass('box6-ext');
+      // クリック処理を実施
+    } else {
+      clickFlg = true;
+      $('.box6').removeClass('box6-ext');
+    // イベント処理中は処理しない
+    return false;
+    }
+  });
+});
