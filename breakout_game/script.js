a4eaa7273c515fce1a33c6c56af86707b79bff42
EC2 Default User <amrumineq@gmail.com>
2023-02-10 12:02:20 +0000

    practice

--- breakout_game/script.js
+++ breakout_game/script.js
@@ -27,14 +27,74 @@ let x = canvas.width / 2;
 let y = canvas.height -30; // y軸が-になるのはキャンバスy軸は下方向に+になるため。詳しくは、mdn「キャンバスでの図形の描画」を見る。
 let dx = 2;
 let dy = -2;
+const ballRadius = 10; // ボールの半径の定数
+
+// パドルの設定
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e){
+  if (e.key === "Right" || e.key === "ArrowRight"){
+    rightPressed = true ;
+  }else if (e.key === "Left" || e.key === "ArrowLeft"){
+    leftPressed = true ;
+  }
+}
+
+function keyUpHandler(e){
+  if (e.key === "Right" || e.key === "ArrowRight"){
+    rightPressed = false ;
+  }else if (e.key === "Left" || e.key === "ArrowLeft"){
+    leftPressed = false ;
+  }
+}
+
+
+// ボールの描画
+function drawBall(){
+  ctx.beginPath();
+  ctx.arc(x,y,ballRadius,0,Math.PI*2);
+  ctx.fillStyle = "#009500";
+  ctx.fill();
+  ctx.closePath();
+}
+
 
 function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  
+  x += dx;
+  y += dy;
+  if (x + dx > canvas.width-ballRadius ||x+ dx < ballRadius){
+    dx = -dx ;
+  }
+  if (y + dy > canvas.height-ballRadius ||y + dy < ballRadius) {
+    dy = -dy;
+  }
+  
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 4,canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 4,0);
+  }
+  
+}
+
+function drawPaddle(){
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.rect(paddleX,canvas.height - paddleHeight,paddleWidth,paddleHeight);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
-  x += dx;
-  y += dy;
 }
+
 setInterval(draw, 10);
